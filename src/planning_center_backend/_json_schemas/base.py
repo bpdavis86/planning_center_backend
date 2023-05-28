72bac8defb9fea15cfbc1c69dd727583a398bb64
Benjamin Davis <benjamin.peter.davis@gmail.com>
2023-05-28 14:47:47 -0500

    Move base API JSON objects to separate file

---
+++ src/planning_center_backend/_json_schemas/base.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import Optional
+
+import msgspec
+
+# region Base Objects
+
+
+class _ApiBase(msgspec.Struct):
+    data: dict
+    included: list
+    meta: dict
+    links: Optional[dict] = None
+
+
+class _DataBase(msgspec.Struct, forbid_unknown_fields=True):
+    type: str
+    id: str
+    attributes: dict
+    links: Optional[dict] = None
+    relationships: Optional[dict] = None
+
+
+# endregion
--- src/planning_center_backend/_json_schemas/groups.py
+++ src/planning_center_backend/_json_schemas/groups.py
@@ -5,25 +5,8 @@ from typing import Optional
 
 import msgspec
 
+from .base import _ApiBase, _DataBase
 
-# region Base Objects
-
-class _ApiBase(msgspec.Struct):
-    data: dict
-    included: list
-    meta: dict
-    links: Optional[dict] = None
-
-
-class _DataBase(msgspec.Struct, forbid_unknown_fields=True):
-    type: str
-    id: str
-    attributes: dict
-    links: Optional[dict] = None
-    relationships: Optional[dict] = None
-
-
-# endregion
 
 # region Groups
 
